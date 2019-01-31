b347130ca874b77ddb0c9d8ca1286653025c8709
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-01-31 16:40:04 +0100

    Update documentation

--- connector_jira_servicedesk/__manifest__.py
+++ connector_jira_servicedesk/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'JIRA Connector ServiceDesk',
+    'name': 'JIRA Connector - Service Desk Extension',
     'version': '11.0.1.0.0',
     'author': 'Camptocamp,Odoo Community Association (OCA)',
     'license': 'AGPL-3',
