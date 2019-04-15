61d14ba77323b1743509ee59d81f0a553a6efce8
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-04-15 17:11:36 +0200

    fixup! Force reimport of worklogs as admin

--- connector_jira/models/account_analytic_line/common.py
+++ connector_jira/models/account_analytic_line/common.py
@@ -100,8 +100,8 @@ class JiraAccountAnalyticLine(models.Model):
 
     @api.multi
     def force_reimport(self):
-        for binding in self.mapped('jira_bind_ids'):
-            binding.sudo().with_delay(priority=8).import_record(
+        for binding in self.sudo().mapped('jira_bind_ids'):
+            binding.with_delay(priority=8).import_record(
                 binding.backend_id,
                 binding.jira_issue_id,
                 binding.external_id,
